e5c063bdd20f000cba1482324d9cbcd39fa3eb39
Joel Lefkowitz <joellefkowitz@hotmail.com>
2020-12-18 19:04:13 +0000

    Add break_str option

--- simple_pipes/pipes.py
+++ simple_pipes/pipes.py
@@ -2,7 +2,7 @@ import subprocess
 from io import TextIOWrapper
 
 
-def pipe_call(call, cwd="."):
+def pipe_call(call, cwd=".", break_str=None):
     p = subprocess.Popen(
         call, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=cwd
     )
@@ -10,6 +10,8 @@ def pipe_call(call, cwd="."):
     for line in TextIOWrapper(p.stdout, encoding="utf-8"):
         print(line, end="")
 
+        if break_str and break_str in line:
+            break
 
 def pipe_capture(call, cwd="."):
     lines = subprocess.check_output(call, cwd=cwd)
